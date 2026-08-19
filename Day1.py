2ab08c595ada7efe640a0fabab26ed581a14bba0
suryakarthika2912 <suryakarthika2912@gmail.com>
2026-08-19 15:05:41 +0530

    Day2

--- Day1.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter a number1:"))
-if a % 2==0:
-    print("The number is even")
-else:
-    print("The number is odd")
-       
---
+++ Day2.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number1: "))
+b = int(input("Enter a number2: "))
+c = int(input("Enter a number3: "))
+if a>b and a>c:
+    print("a is the largest number")
+elif b>a and b>c:
+    print("b is the largest number")
+else:
+    print("c is the largest number")        
